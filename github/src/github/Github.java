5c79e2cc0675c14b1bd5cf839358b628efcb2269
USUARIO <USUARIO@DESKTOP-AHEH8A4>
2021-05-31 14:51:10 -0500

    version 3 modificamos el System

--- github/src/github/Github.java
+++ github/src/github/Github.java
@@ -7,7 +7,7 @@ public class Github {
    
     public static void main(String[] args) {
         System.out.println("Taller de conexión GitHub ");
-        system.out.println("version 2");
+        System.out.println("version 2");
     }
     
 }
