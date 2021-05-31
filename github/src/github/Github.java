3e561e49236c38a05ae0ef52ddd808fba1eaa2ee
SergioPenaloza <85132050+SergioPenaloza@users.noreply.github.com>
2021-05-31 14:45:13 -0500

    Update Github.java

--- github/src/github/Github.java
+++ github/src/github/Github.java
@@ -7,6 +7,7 @@ public class Github {
    
     public static void main(String[] args) {
         System.out.println("Taller de conexión GitHub ");
+        system.out.println("version 2");
     }
     
 }
